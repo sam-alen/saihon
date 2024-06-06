578e21a1670c0a79bf0455e47c45b90ec59af19d
balejoc <bralioiac@gmail.com>
2024-06-06 15:53:19 -0600

    Se ajustó el CSS de la navbar para todas las páginas, exceptuando contáctanos y nosotros

--- js/contactanos.js
+++ js/contactanos.js
@@ -109,31 +109,31 @@ function addNavbar(header){
     header.insertAdjacentHTML('afterbegin', `
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Saihon</a>
+            <a class="navbar-brand" href="#"><h2 id="logo">Saihon</h2></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="./index.html">Inicio</a>
+                  <a class="nav-link active otrosL" aria-current="page" href="./index.html">Inicio</a>
                 </li>
                 <p class="separador">|</p>
                 
                 <li class="nav-item">
-                  <a class="nav-link" href="./catalogo.html">Catálogo</a>
+                  <a class="nav-link otrosL" href="./catalogo.html">Catálogo</a>
                 </li>
                 <p class="separador">|</p>
                 
                 <li class="nav-item">
-                  <a class="nav-link" href="./contactanos.html">Contáctanos</a>
+                  <a class="nav-link otrosL" href="./contactanos.html">Contáctanos</a>
                 </li>
                 <p class="separador">|</p>
                 
                 <li class="nav-item">
-                  <a class="nav-link"  href="./nosotros.html">Nosotros</a>
+                  <a class="nav-link otrosL"  href="./nosotros.html">Nosotros</a>
                 </li>
-                <p class="separador">|</p>
+                
                 
               </ul>
             </div>
