06902d0971ccb53f9f9b2b482f91f16c91e85888
Jalil <jalil.mendoza.r@gmail.com>
2024-06-11 15:44:25 -0600

    Se actualizaron las cards y se colocaron los modales

--- js/catalogo.js
+++ js/catalogo.js
@@ -144,10 +144,39 @@ function addBooks(libros){
       <div class="card-body">
         <h5 class="card-title">${libro.title}</h5>
         <p class="card-text"> ${libro.author}</p>        
-        <p class="card-text"> $${libro.price}</p>
-        <a href="#" class="btn btn-primary">Ver más</a>
+        <p class="card-text"> ${libro.price}$</p>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal_${libro.id}">
+          Ver más
+        </button>
+
       </div>
-    </div>`)
+    </div>
+    <!-- Modal -->
+    <div class="modal fade modal-dialog modal-dialog-centered modal-dialog-scrollable" id="exampleModal_${libro.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body ">
+            <h2> ${libro.title}</h2>
+            <h3>${libro.author}</h3>
+            <h3>${libro.publication_year}</h3> 
+            <h3>${libro.genre}</h3>
+            <p>${libro.description}</p>
+            <h4>${libro.price}$</h4>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `)
+
     
   });
 }
