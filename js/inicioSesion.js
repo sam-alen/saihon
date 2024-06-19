d7fc64fdb4770637887e4253196ee32a3845810d
balejoc <bralioiac@gmail.com>
2024-06-18 23:39:00 -0600

    Se agregó la funcionalidad completa para la validación de los campos del registro

--- js/inicioSesion.js
+++ js/inicioSesion.js
@@ -10,8 +10,7 @@ const btn = document.getElementsByClassName("btn");
 const submitRegistro = document.getElementById("submitSupremo");
 
 const errorCampos = document.getElementById("errorCampos");
-
-let errorString = "Error: "; 
+errorCampos.style.color = "crimson";
 let user = [];
 
 const reName = RegExp(/[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,50}$/);
@@ -21,61 +20,81 @@ const rePhone = /(?:\+52\s?)?(\(?\d{2,3}\)?)?(\s|-)?(\d{4})(\s|-)?(\d{4})/;
 
 //Validaciones version 1
 
-function validarNombre(){
-     let reName = RegExp(/[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,50}$/);
-     return reName.test(nombre.value.trim());
-}
+// function validarNombre(){
+//      let reName = RegExp(/[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,50}$/);
+//      return reName.test(nombre.value.trim());
+// }
 
-function validarEmail(){
-    let reMail = RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
-    return reMail.test(email.value.trim());
- }
+// function validarEmail(){
+//     let reMail = RegExp(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/);
+//     return reMail.test(email.value.trim());
+//  }
 
- // contraseña de 8 caracteres minimo, una mayus, una minus, un caracter especial y un numero
- function validarPassword(){
-    let rePass = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
-     return (contrase.value.trim() === confirmacion.value.trim() && rePass.test(contrase.value.trim()));
+//  // contraseña de 8 caracteres minimo, una mayus, una minus, un caracter especial y un numero
+//  function validarPassword(){
+//     let rePass = RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
+//      return (contrase.value.trim() === confirmacion.value.trim() && rePass.test(contrase.value.trim()));
 
- }
+//  }
 
- function validarTelefono(){
-    let rePhone = /(?:\+52\s?)?(\(?\d{2,3}\)?)?(\s|-)?(\d{4})(\s|-)?(\d{4})/;
-    return telefono.value.trim().match(rePhone);
-}
+//  function validarTelefono(){
+//     let rePhone = /(?:\+52\s?)?(\(?\d{2,3}\)?)?(\s|-)?(\d{4})(\s|-)?(\d{4})/;
+//     return telefono.value.trim().match(rePhone);
+// }
 
 function crearUsuario(){
-  errorCampos.innerHTML = "";
+  // Limpiamos los errores 
+  let errorString = "";
+  errorCampos.innerHTML = " ";
   nombre.style.border = "none";
   email.style.border = "none";
   contrase.style.border = "none";
   telefono.style.border = "none";
+  confirmacion.style.border = "none";
 
+  let nombreValido = true;
   if (!reName.test(nombre.value.trim())) {
     // errorNombre.innerHTML = "Por favor ingresa un nombre válido (al menos 3 caracteres)";
-    errorString += " Nombre incorrecto";
+    errorString += " Nombre incorrecto <br>";
     nombre.style.border = "2px solid crimson";
+    nombreValido = false;
   }
 
+  let emailValido = true;
   if (!reMail.test(email.value.trim())) {
       // errorEmail.innerHTML = "Por favor ingresa un email válido";
-      errorString += " Correo incorrecto";
+      errorString += " Correo incorrecto <br>";
       email.style.border = "2px solid crimson";
+      emailValido = false;
   }
 
+  let contraValida = true;
   if (!rePass.test(contrase.value.trim())) {
       // errorTelefono.innerHTML = "Por favor ingresa un teléfono válido (entre 10 y 12 dígitos)";
-      errorString += " Telefóno incorrecto";
+      errorString += " Contraseña incorrecta <br>";
       contrase.style.border = "2px solid crimson";
+      contraValida = false;
   }
 
+  let contraIguales = true;
+  if (contrase.value.trim() != confirmacion.value.trim()){
+    errorString += " Las contraseñas no coinciden <br>";
+    confirmacion.style.border = "2px solid crimson";
+    contraIguales = false;
+  }
+
+  console.log(contrase.value.trim());
+  console.log(confirmacion.value.trim());
+  let telefonoValido = true;
   if (!telefono.value.trim().match(rePhone)) {
     // errorTelefono.innerHTML = "Por favor ingresa un teléfono válido (entre 10 y 12 dígitos)";
-    errorString += " Telefóno incorrecto";
+    errorString += " Telefono incorrecto <br>";
     telefono.style.border = "2px solid crimson";
-}
+    telefonoValido = false;
+  }
 
-    if (validarNombre() && validarEmail() && validarPassword() && validarTelefono()){
-      // let user = [];      
+    // if (validarNombre() && validarEmail() && validarPassword() && validarTelefono()){
+    if (nombreValido && emailValido && contraValida && telefonoValido && contraIguales){ 
       
       if (localStorage.getItem("user") != null){
         user = JSON.parse(localStorage.getItem("user"));
@@ -101,7 +120,7 @@ function crearUsuario(){
         timer: 1500
       });
     } else {
-      errorCampos.insertAdjacentHTML("beforebegin",errorString);
+      errorCampos.innerHTML= errorString;
     }
 }
 
