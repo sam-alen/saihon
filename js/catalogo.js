e8c5eb5da4b86cede913f766715f30a764e49537
ingrid <archieninetyeight@hotmail.com>
2024-06-26 17:35:53 -0600

    Ajustes en la página de catálogo, se ajustaron libros a las categorías correspondientes.

--- js/catalogo.js
+++ js/catalogo.js
@@ -17,7 +17,8 @@ addFooter(footer);
 
 
 const main = document.getElementById("main");
-const seccionLibro_todos= document.getElementById("seccionLibro-todos")
+const seccionLibro_todos= document.getElementById("seccionLibro-todos");
+
 
 let librosLocalStorage = [];
 
@@ -166,24 +167,50 @@ if (localStorage.getItem("librosLocalStorage") != null){
 
 function addBooks(libros){
   libros.forEach(libro => {
-    seccionLibro_todos.insertAdjacentHTML("beforeend", 
-      `
-        <div class="card" style="width: 18rem;">
-          <img src=${libro.cover_image} class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${libro.title}</h5>
-            <p class="card-text"> ${libro.author}</p>        
-            <p class="card-text"> ${libro.price}$</p>
-            <div class="container-botones-card">
-                  <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary boton" data-bs-toggle="modal" data-bs-target="#exampleModal_${libro.id}">
-                  Ver más
-                </button>
-                  <img id="carrito_${libro.id}" class="carrito-svg-card" src="assets/imagenes/logo/carrito2.svg" alt="">
-            </div>
-          </div>
+
+    seccionLibro_todos.insertAdjacentHTML("beforeend", generateBooksHTML(libro));
+
+
+libro.genre.forEach(g=> {
+  let seccionLibro_categoria;
+  switch (g.toLowerCase()){
+    case 'fiction':
+    case 'science fiction':
+    case 'ciencia ficcion':
+      seccionLibro_categoria = document.getElementById("seccionLibro-CF");
+    break;
+    case 'romance':
+      seccionLibro_categoria = document.getElementById("seccionLibro-romance");
+    break;
+    case 'terror':
+      seccionLibro_categoria = document.getElementById("seccionLibro-terror");
+    break;
+    default:
+      return;
+  }
+  seccionLibro_categoria.insertAdjacentHTML("beforeend", generateBooksHTML(libro));
+
+     });
+  });
+}
+
+function generateBooksHTML(libro){
+  return `
+  <div class="card" style="width: 18rem;">
+      <img src="${libro.cover_image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${libro.title}</h5>
+        <p class="card-text">${libro.author}</p>        
+        <p class="card-text">${libro.price}$</p>
+        <div class="container-botones-card">
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-primary boton" data-bs-toggle="modal" data-bs-target="#exampleModal_${libro.id}">
+            Ver más
+          </button>
+          <img id="carrito_${libro.id}" class="carrito-svg-card" src="assets/imagenes/logo/carrito2.svg" alt="">
         </div>
-        
+      </div>
+    </div>
     <!-- Modal -->
     <div class="modal fade" id="exampleModal_${libro.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -192,10 +219,10 @@ function addBooks(libros){
             <h1 class="modal-title fs-5" id="exampleModalLabel">${libro.title}</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          <div class="modal-body ">
+          <div class="modal-body">
             <h3>${libro.author}</h3>
-            <h3>${libro.publication_year}</h3> 
-            <h3>${libro.genre}</h3>
+            <h3>${libro.publication_year}</h3>
+            <h3>${libro.genre.join(', ')}</h3>
             <p>${libro.description}</p>
             <h4>${libro.price}$</h4>
           </div>
@@ -206,7 +233,6 @@ function addBooks(libros){
         </div>
       </div>
     </div>
-    `)
-  });
+  `;
 }
 
